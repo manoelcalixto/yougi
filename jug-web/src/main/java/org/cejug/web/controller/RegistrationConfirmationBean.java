c26f5266531cefa8015539223217d8e7d82d1198
Hildeberto Mendonca <me@hildeberto.com>
2013-01-19 12:18:23 +0100

    Bug on the registration of the first user fixed.

--- jug-web/src/main/java/org/cejug/web/controller/RegistrationConfirmationBean.java
+++ jug-web/src/main/java/org/cejug/web/controller/RegistrationConfirmationBean.java
@@ -1,21 +1,21 @@
-/* Jug Management is a web application conceived to manage user groups or 
- * communities focused on a certain domain of knowledge, whose members are 
- * constantly sharing information and participating in social and educational 
+/* Jug Management is a web application conceived to manage user groups or
+ * communities focused on a certain domain of knowledge, whose members are
+ * constantly sharing information and participating in social and educational
  * events. Copyright (C) 2011 Ceara Java User Group - CEJUG.
- * 
- * This application is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by the 
- * Free Software Foundation; either version 2.1 of the License, or (at your 
+ *
+ * This application is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
  * option) any later version.
- * 
- * This application is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ *
+ * This application is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
- * 
- * There is a full copy of the GNU Lesser General Public License along with 
+ *
+ * There is a full copy of the GNU Lesser General Public License along with
  * this library. Look for the file license.txt at the root level. If you do not
- * find it, write to the Free Software Foundation, Inc., 59 Temple Place, 
+ * find it, write to the Free Software Foundation, Inc., 59 Temple Place,
  * Suite 330, Boston, MA 02111-1307 USA.
  * */
 package org.cejug.web.controller;
@@ -45,9 +45,9 @@ public class RegistrationConfirmationBean {
 
     @ManagedProperty(value="#{param.code}")
     private String code;
-    
+
     private String informedCode;
-    
+
     private Boolean validated;
 
     public RegistrationConfirmationBean() {
@@ -91,10 +91,10 @@ public class RegistrationConfirmationBean {
     public void setValidated(Boolean validated) {
         this.validated = validated;
     }
-    
+
     /**
-     * Validates the registration when the confirmation code comes 
-     * through querystring.
+     * Validates the registration when the confirmation code comes
+     * through query string.
      */
     @PostConstruct
     public void load() {
@@ -102,17 +102,18 @@ public class RegistrationConfirmationBean {
             FacesContext context = FacesContext.getCurrentInstance();
             ResourceBundleHelper bundle = new ResourceBundleHelper();
             this.userAccount = userAccountBsn.confirmUser(this.code);
-            if(this.userAccount != null)
+            if(this.userAccount != null) {
                 this.validated = Boolean.TRUE;
+            }
             else {
                 this.validated = Boolean.FALSE;
                 context.addMessage(this.informedCode, new FacesMessage(FacesMessage.SEVERITY_WARN, bundle.getMessage("warnCode0003"), ""));
             }
         }
     }
-    
+
     /**
-     * Validates the registration when the confirmation code is 
+     * Validates the registration when the confirmation code is
      * manually informed.
      */
     public String confirmUser() {
@@ -122,7 +123,7 @@ public class RegistrationConfirmationBean {
                 this.validated = Boolean.TRUE;
             else
                 this.validated = Boolean.FALSE;
-            
+
             FacesContext context = FacesContext.getCurrentInstance();
             ResourceBundleHelper bundle = new ResourceBundleHelper();
             if(!this.validated)
