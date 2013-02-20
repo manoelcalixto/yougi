b0b60d25a0cbe5ca30038f9e35a8eef65d2c4eb0
Hildeberto Mendonca <me@hildeberto.com>
2013-02-20 17:26:16 +0100

    Project renamed and refactored from JugManament to Yougi, a variation of UG.

--- jug-ejb/src/main/resources/META-INF/mysql-queries.sql
+++
@@ -1,5 +0,0 @@
-# Shows users and their correspondent subscriptions in the mailing_list
-select ua.id, ua.email, mls.email_address, cp.mailing_list, mls.unsubscription_date
-from user_account ua join mailing_list_subscription mls on ua.email = mls.email_address
-                     join communication_privacy cp on ua.id = cp.user
-where ua.deactivated = 0 and ua.confirmation_code is null;
